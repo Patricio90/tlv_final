bfe3fd86031a42302fb52965a49bd190755cf0ad
Patricio90 <90427375+Patricio90@users.noreply.github.com>
2022-07-04 22:54:00 -0400

    updates views about and contact

--- appfarm/urls.py
+++ appfarm/urls.py
@@ -1,6 +1,9 @@
+from unicodedata import name
 from .import views 
 from django.urls import path
 
 urlpatterns = [
-    path('', views.inicio, name ='inicio')
+    path('', views.inicio, name ='inicio'),
+    path('about/', views.about, name = 'about-appfarm'),
+    path('contacto/',views.contacto, name = 'appfarm-contacto')
 ]
