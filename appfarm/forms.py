10eb97c7adf460b24aae623af6838fd6b9379c41
Mauricio Aspee <ale_aspee@hotmail.com>
2022-07-12 00:22:19 -0400

    V4 Crud
    Implementación de Crud de productos, listado de productos.

---
+++ appfarm/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Producto, Categoria
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'categoria', 'precio', 'imagen']
--- appfarm/urls.py
+++ appfarm/urls.py
@@ -1,7 +1,16 @@
 from .import views 
 from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('', views.inicio, name ='inicio'),
-    path('nosotros', views.nosotros, name='nosotros')
-]
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('productos', views.productos, name='productos'),
+    path('lista', views.listar, name='lista'),
+    path('productos/crear_productos', views.crear_producto, name='crear'),
+    path('productos/editar_productos', views.editar_producto, name='editar'),
+    path('eliminar/<int:id>', views.eliminar, name='eliminar'),
+    path('productos/editar/<int:id>', views.editar_producto, name='editar'),
+
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
